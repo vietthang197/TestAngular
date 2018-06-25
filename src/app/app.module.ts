5ecb07fd6110252a00daa6ee2d9750d747e981a3
Lê Việt Thắng <levietthang1997@gmail.com>
2018-06-25 14:58:27 +0700

    mother fucker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { WordComponent } from './word/word.component';
+import { BookComponent } from './book/book.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+      WordComponent,
+      BookComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/word/word.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    templateUrl: './word.component.html',
+    selector: 'app-word',
+    styleUrls: ['./word.component.css']
+})
+export class WordComponent {
+      motherfucker = 'fucker';
+}
+
